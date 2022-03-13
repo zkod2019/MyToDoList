06691d324655c57cbf7706e69896ee9c5fcc3d7a
Zaida Kodzoeva <zkod777@gmail.com>
2022-03-13 13:36:37 +0400

    Learning about props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Greeting(props) {
+  return <h1>Hello {props.name}</h1>
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Hello React!</h1>
+      <Greeting name="Zaya" />
+    </main>
   );
+
+  /*
+
+  JSX is just sugar for function call syntax.
+
+  The above code ^ would compile to something like:
+
+    return (
+      React.createElement(
+        "main",
+        {},
+        React.createElement(
+          "h1",
+          {},
+          "Hello React!"
+        )
+      )
+    )
+
+    The parameters are:
+      React.createElement(tag, props, ...children)
+
+  <h1 class="pain" id="suffering">Hello</h1>
+          React.createElement(
+          "h1",
+          {class: "pain", id: "suffering"},
+          "Hello React!"
+        )
+
+  */
 }
 
 export default App;
